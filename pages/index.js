04070cdbdafff96d7df05dcffdad77501dff9f99
iammasterbrucewayne <iammasterbrucewayne@protonmail.com>
2023-04-22 13:27:40 +0530

    bug fixes

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,13 @@ import Roadmap from "@components/Roadmap/Roadmap";
 import Contact from "@components/Contact/Contact";
 import Disclaimer from "@components/Disclaimer/Disclaimer";
 import { Box } from "@chakra-ui/react";
+import { isMobile } from "react-device-detect";
 
 export default function Home() {
 	return (
 		<Box
 			bg="#FFE503"
+			w={isMobile ? "fit-content" : "full"}
 			px={12}
 			fontFamily="'Gochi Hand', cursive"
 			color="#363015"
